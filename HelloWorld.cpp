364a6ae07b7f93e87d11023df74a4b8fa29e71c0
NdTurner <ndturner@mail.bradley.edu>
2022-06-05 09:46:25 -0500

    init

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,c;
+
+    cout<< "input numbers / output addition" <<endl;
+    cin >> a >> b >> c;
+	cout << "Hello world!: " << (a+b+c) <<endl;
+	
+	return 0;
+}
---
+++ JackOLanternJuxtaposition.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void){
+
+ int N;
+ int T;
+ int M;
+
+ cin>>N >> T >> M;
+
+cout << (N*M)*T;
+
+
+}
